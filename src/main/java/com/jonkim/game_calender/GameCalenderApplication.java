6d32b78be41bddd0e972e4cd4d4eef8461428406
HelloJon29 <jonkim1996@gmail.com>
2023-02-24 21:27:09 -0600

    Bootstrapped spring backend with JPA support

---
+++ src/main/java/com/jonkim/game_calender/GameCalenderApplication.java
@@ -0,0 +1,14 @@
+package com.jonkim.game_calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCalenderApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
