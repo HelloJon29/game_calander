2ea8a810f7051889639ad60fd15873fbb7af885e
HelloJon29 <jonkim1996@gmail.com>
2023-02-25 18:20:44 -0600

    Updated intializer sql script

--- src/main/sql/initialize_tables_20230225.sql
+++ src/main/sql/initialize_tables_20230225.sql
@@ -1,17 +1,17 @@
 -- create tables
+CREATE TABLE IF NOT EXISTS thumbnails (
+	id INT NOT NULL AUTO_INCREMENT,
+    CONSTRAINT thumbnails_PK PRIMARY KEY (id)
+    );
+    
 CREATE TABLE IF NOT EXISTS games (
 	id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     description VARCHAR(255),
     release_date DATE,
-    CONSTRAINT games_PK PRIMARY KEY (id)
-    );
-    
-CREATE TABLE IF NOT EXISTS thumbnails (
-	id INT NOT NULL AUTO_INCREMENT,
-    game_id INT NOT NULL,
-    CONSTRAINT thumbnails_PK PRIMARY KEY (id),
-    CONSTRAINT thumbnails_FK FOREIGN KEY (game_id) REFERENCES games(id)
+    thumbnail_id INT,
+    CONSTRAINT games_PK PRIMARY KEY (id),
+    CONSTRAINT games_FK FOREIGN KEY (thumbnail_id) REFERENCES thumbnails(id)
     );
     
 CREATE TABLE IF NOT EXISTS screen_shots (
@@ -28,16 +28,16 @@ ALTER TABLE game_calender.games
 	MODIFY COLUMN id INT COMMENT 'This is the primary key used to identify unique records of games',
 	MODIFY COLUMN name VARCHAR(255) COMMENT 'The name of the video game',
 	MODIFY COLUMN description VARCHAR(255) COMMENT 'A description of the video game',
-    MODIFY COLUMN release_date DATE COMMENT 'The release date info of the video game';
+    MODIFY COLUMN release_date DATE COMMENT 'The release date info of the video game',
+    MODIFY COLUMN thumbnail_id DATE COMMENT 'Foreign key reference to thumbnails table, 1-1 relation';
     
 -- thumbnails
 ALTER TABLE game_calender.thumbnails COMMENT='Main table used to store the thumbnail for a video game entry';
 ALTER TABLE game_calender.thumbnails
-	MODIFY COLUMN id INT COMMENT 'This is the primary key used to identify unique records of thumbnails',
-	MODIFY COLUMN game_id INT COMMENT 'This is a foreign key reference to games id to connect the tables';
+	MODIFY COLUMN id INT COMMENT 'This is the primary key used to identify unique records of thumbnails';
 
 -- screenshots
 ALTER TABLE game_calender.screen_shots COMMENT='Main table used to store the thumbnail for a video game entry';
 ALTER TABLE game_calender.screen_shots
 	MODIFY COLUMN id INT COMMENT 'This is the primary key used to identify unique records of screenshots',
-	MODIFY COLUMN game_id INT COMMENT 'This is a foreign key reference to games id to connect the tables';
+	MODIFY COLUMN game_id INT COMMENT 'This is a foreign key reference to games id to connect the tables 1-many relation';
