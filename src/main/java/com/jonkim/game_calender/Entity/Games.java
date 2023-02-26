068f570630088c2c3688265ef10b6fe06d3ee418
HelloJon29 <jonkim1996@gmail.com>
2023-02-25 18:44:45 -0600

    Finished initial mockup of Games entity

--- src/main/java/com/jonkim/game_calender/Entity/Games.java
+++ src/main/java/com/jonkim/game_calender/Entity/Games.java
@@ -1,27 +1,120 @@
 package com.jonkim.game_calender.Entity;
 
-import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Games")
+@Table(name = "games")
 public class Games {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, columnDefinition = "INT")
     private long id;
-    @Column(name = "name", nullable = false, columnDefinition = "VARCHAR")
+
+    @Column(name = "name", columnDefinition = "VARCHAR", length = 255)
     private String name;
+
+    @Column(name = "description", columnDefinition = "VARCHAR", length = 255)
     private String description;
-    private Date releaseDate;
+
+    @Column(name = "release_date", columnDefinition = "DATE")
+    private LocalDate releaseDate;
+
     @Lob
+    @Column(name = "thumbnail_id", columnDefinition = "INT")
+    @OneToMany
     private byte[] thumbnail;
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public byte[] getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(byte[] thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    // hashCode and equals overrides
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((releaseDate == null) ? 0 : releaseDate.hashCode());
+        result = prime * result + Arrays.hashCode(thumbnail);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Games other = (Games) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (releaseDate == null) {
+            if (other.releaseDate != null)
+                return false;
+        } else if (!releaseDate.equals(other.releaseDate))
+            return false;
+        if (!Arrays.equals(thumbnail, other.thumbnail))
+            return false;
+        return true;
+    }
+
 }
